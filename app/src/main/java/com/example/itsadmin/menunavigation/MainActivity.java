6f95b426b645e90d3fd89cab17518df081711d81
Mohamed Salad <msalad@uncc.edu>
2016-10-22 00:13:41 -0400

    Staging for DrawerNavigation

--- app/src/main/java/com/example/itsadmin/menunavigation/MainActivity.java
+++ app/src/main/java/com/example/itsadmin/menunavigation/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.itsadmin.menunavigation;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.activity_main, mPlanetTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
     }
 }
